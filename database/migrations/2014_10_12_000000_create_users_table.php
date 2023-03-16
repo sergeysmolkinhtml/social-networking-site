687d256390b87bb57519df5fac656767740e6427
Sergiy <37virs85@gmail.com>
2023-03-16 22:49:39 +0200

    users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,34 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
+            $table->string('nickname')->nullable();
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('city')->nullable();
+
+            $table->string('password',60);
+
+            $table->date('date_of_birth')->nullable();
+            $table->string('status_description')->nullable();
+            $table->timestampTz('last_visited_at')->nullable();
+            $table->ipAddress('last_visited_from')->nullable();
+
+            $table->boolean('active')->default(1);
+            $table->string('verification_token',100)->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
+
+
+
         });
     }
 
