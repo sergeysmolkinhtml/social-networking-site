45b80f165eeea541dc7948a10e5f4ed1b735fd79
Sergiy <37virs85@gmail.com>
2023-03-18 20:14:25 +0200

    posts list [not finished]

--- app/Http/Controllers/NewsPage.php
+++ app/Http/Controllers/NewsPage.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BlogPost;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NewsPage extends Controller
 {
     public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
+        if (Auth::check()) {
+            $posts = BlogPost::where(function ($query) {
+                return $query->where('user_id', Auth::user()->id)
+                    ->orWhereIn('user_id', Auth::user()->friends()->pluck('id'));
+            })->orderBy('created_at', 'asc')
+                ->paginate(10);
+
+            return view('news.news',compact('posts'));
+        }
+
         return view('news.news');
     }
 }
--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -46,4 +46,77 @@
         </div>
 
     </main>
+
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Recommendations</h1>
+        </div>
+    </header>
+
+    <main>
+    @if(!$posts->count())
+            <form action="{{route('blog.post')}}" method="post">
+                @csrf
+                <button type="submit" class="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">
+                    Talk something
+                </button>
+            </form>
+    @else
+        @foreach($posts as $post)
+        <div class="bg-white py-24 sm:py-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                <div class="mx-auto max-w-2xl lg:mx-0">
+                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{$post->title}}</h2>
+
+                    <div class="flex items-center">
+                        <button class="flex items-center justify-center w-12 h-12 rounded-full bg-gray-200 hover:bg-pink-500 hover:text-white transition-colors duration-300 focus:outline-none focus:bg-pink-500 focus:text-white"
+                            aria-label="Like"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M12 5l7 7-7 7"/>
+                            </svg>
+                        </button>
+                        <p class="ml-3 text-gray-700">Нравится</p>
+                    </div>
+                </div>
+                <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                    <article class="flex max-w-xl flex-col items-start justify-between">
+                        <div class="flex items-center gap-x-4 text-xs">
+                            <time datetime="2020-03-16" class="text-gray-500">date publication</time>
+                            <a href="#" class="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100">category</a>
+                        </div>
+                        <div class="group relative">
+                            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                <a href="#">
+                                    <span class="absolute inset-0"></span>
+                                    first words from description
+                                </a>
+                            </h3>
+                            <p class="mt-5 text-sm leading-6 text-gray-600 line-clamp-3">{{$post->content_raw}}</p>
+                        </div>
+
+                        <div class="relative mt-8 flex items-center gap-x-4">
+                           <a href="">
+
+                               <img src="{{$post->user->profilePictureUrl()}}" alt="" class="h-10 w-10 rounded-full bg-gray-50">
+                           </a>
+                            <div class="text-sm leading-6">
+                                <p class="font-semibold text-gray-900">
+                                    <a href="{{route('user_profile.index',$post->user->nickname)}}">
+                                        <span class="absolute inset-0"></span>
+                                        Mauthor
+                                    </a>
+                                </p>
+                                <p class="text-gray-600">job title</p>
+                            </div>
+                        </div>
+                    </article>
+
+                    <!-- More posts... -->
+                </div>
+            </div>
+        </div>
+    </main>
+    @endforeach
+    @endif
 @endsection
